8f3c34991c77c40230ac325468a78a02475f7211
David Rueck <drueck@gmail.com>
2013-05-22 23:11:34 -0700

    Initial commit. New Rails 4 app with Postgresql and Rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Games::Application.config.secret_key_base = '02c4425d39aaef1bf6e956c64a48a7ec289c7d98e1a9e741ba15fbd8517226f68cf949d2833cc59529274e3684b94bb7b24dbba002533ebc57fd28e41b21cac0'
