29855551a085a8f5d8919e31392c212103ddadec
David Rueck <drueck@gmail.com>
2013-05-27 19:43:59 -0700

    Use Foundation alert boxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def foundation_alert_class(flash_type)
+    case flash_type
+    when :notice
+      'success'
+    when :error
+      'alert'
+    else
+      ''
+    end
+  end
+
 end
