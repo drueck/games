1874d9e43a0e5c01979b86d41120106f1d99e9d4
David Rueck <drueck@gmail.com>
2013-05-27 17:12:00 -0700

    Installed Devise. Copied views, generated User model + migrations, etc.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Games::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,4 +26,7 @@ Games::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Devise said I needed this
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
