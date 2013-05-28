1553c32e79785f8bf3d3d8b6d8fbfc19ab32d215
David Rueck <drueck@gmail.com>
2013-05-27 21:09:46 -0700

    Add game model and simple initial test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130525175324) do
+ActiveRecord::Schema.define(version: 20130528030539) do
+
+  create_table "games", force: true do |t|
+    t.string   "title",      null: false
+    t.integer  "bgg_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/games.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :game do
+    title "MyString"
+    bgg_id 1
+  end
+end
