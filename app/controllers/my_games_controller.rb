d6e110a7317313831e906225d520d73c91992994
David Rueck <drueck@gmail.com>
2013-05-28 23:15:23 -0700

    Add initial implementation of MyGamesController#index

--- app/controllers/my_games_controller.rb
+++ app/controllers/my_games_controller.rb
@@ -3,6 +3,7 @@ class MyGamesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @games = current_user.games
   end
 
 end
--- spec/controllers/my_games_controller_spec.rb
+++ spec/controllers/my_games_controller_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe MyGamesController do
 
+  include Devise::TestHelpers
+
+  describe "GET index" do
+
+    before(:each) do
+      user_game = FactoryGirl.create(:user_game)
+      @user = user_game.user
+      @game = user_game.game
+      FactoryGirl.create(:game)
+      request.env['warden'].stub :authenticate! => @user
+      controller.stub :current_user => @user
+    end
+
+    it "should set @games to the list of the current user's games" do
+      get :index
+      expect(assigns(:games)).to include(@game)
+      expect(assigns(:games).length).to eq 1
+    end
+
+  end
+
 end
