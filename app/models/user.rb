479bdc7167f7d4d9c95472579cb535a752ca9f8c
David Rueck <drueck@gmail.com>
2013-06-29 12:59:10 -0700

    Improve User add_game functionality
    Add add_game_by_title instead of handling String and Game cases
    in add_game. add_game_by_title checks for an existing game with the
    same title, and if so calls add_game with the existing game. If not,
    it creates a new game with that title, but formats the title nicely.
    This should prevent a bunch of crappy differences in titleization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,19 @@ class User < ActiveRecord::Base
   has_many :games, through: :user_games
 
 	def add_game(game)
-		if game.kind_of? Game
-			games << game
-		else
-			existing_game = Game.where('title ilike ?', game).limit(1).try(:first)
-			g = existing_game || Game.create(title: game)
-			games << g
-		end
+		games << game unless games.include?(game)
+	end
+
+	def add_game_by_title(title)
+		existing_game = Game.where('title ilike ?', title).limit(1).try(:first)
+		game = existing_game || Game.create(title: format_title(title))
+		add_game(game)
+	end
+
+	private
+
+	def format_title(title)
+		title.downcase.titleize
 	end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,49 +5,80 @@ describe User do
   it { should have_many(:user_games).dependent(:destroy) }
   it { should have_many(:games).through(:user_games) }
 
-	describe "#add_game(title_or_game)" do
-		context "when game is a String" do
+	describe "#add_game_by_title(title)" do
 
-			context "when there is no matching game already in the database" do
-				before(:each) do
-					@user = described_class.new
-					@game = 'Game ' + rand(100).to_s
-				end
-				it "should create the Game, setting the title to the string passed in" do
-					expect { @user.add_game(@game) }.to change{ Game.count }.from(0).to(1)
-					expect(Game.first.title).to eq @game
-				end
-				it "should add the newly created game to the user's game collection" do
-					@user.add_game(@game)
-					expect(@user.games).to include(Game.first)
-				end
+		before(:each) do
+			@user = described_class.new
+			@title = 'game nUmbER ' + rand(100).to_s
+		end
+
+		context "when there is no matching game already in the database" do
+
+			it "should create the Game, setting the title to the titleized string passed in" do
+				expect { @user.add_game_by_title(@title) }.to change{ Game.count }.from(0).to(1)
+				expect(Game.first.title).to eq @title.downcase.titleize
 			end
 
-			context "when a game with the same title (differing only in case) is already in the database" do
-				before(:each) do
-					@user = described_class.new
-					@game = Game.create(title: 'Game ' + rand(100).to_s)
-				end
-				it "should not create a new game" do
-					expect { @user.add_game(@game.title.downcase) }.not_to change { Game.count }
-				end
-				it "should add the existing game with the same title to the user's collection" do
-					@user.add_game(@game.title.upcase)
-					expect(@user.games).to include(@game)
-				end
+			it "should add the newly created game to the user's game collection" do
+				@user.add_game_by_title(@title)
+				expect(@user.games).to include(Game.first)
 			end
 
 		end
-		context 'when game is a Game' do
-			before(:each) do
-				@user = described_class.new
-				@game = Game.create(title: 'Game ' + rand(100).to_s)
+
+		context "when a game with the same title (differing only in case) is already in the database" do
+
+			it "should not create a new game" do
+				@user.add_game_by_title(@title.upcase)
+				expect { @user.add_game_by_title(@title.downcase) }.not_to change { Game.count }
 			end
+
+			it "should add the existing game with the same title to the user's collection" do
+				game = Game.create(title: @title.downcase)
+				@user.add_game_by_title(@title.upcase)
+				expect(@user.games).to include(game)
+			end
+
+		end
+
+		context "when a game with the same title (differing only in case) is already in the user's collection" do
+
+			it "should not add a duplicate game" do
+				@user.add_game_by_title(@title.upcase)
+				@user.add_game_by_title(@title.downcase)
+				expect(@user.games.length).to eq 1
+			end
+
+		end
+
+	end
+
+	describe "#add_game(game)" do
+
+		before(:each) do
+			@user = described_class.new
+			@game = Game.create(title: 'Game ' + rand(100).to_s)
+		end
+
+		context "when the game is not already in the user's collection" do
+
 			it "should add the game to the user's game collection" do
 				expect { @user.add_game(@game) }.not_to change { Game.count }
 				expect(@user.games).to include @game
 			end
+
 		end
+
+		context "when the game is already in the user's collection" do
+
+			it "should not add a duplicate game" do
+				@user.add_game(@game)
+				@user.add_game(@game)
+				expect(@user.games.length).to eq 1
+			end
+
+		end
+
 	end
 
 end
